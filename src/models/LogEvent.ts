7e52d1c6e696ac67e5f4d6a3b3300b390e0dd3ac
RAGHAB NEUPANE <neupaneraghab@gmail.com>
2026-06-05 13:36:21 +0545

    SERVICE ADDED: Browser Inormations

--- src/models/LogEvent.ts
+++ src/models/LogEvent.ts
@@ -1,19 +1,38 @@
+// export interface LogEvent {
+//     id: string;
+//     deviceId?: string;
+//     level: "debug" | "info" | "warn" | "error";
+//     message: string;
+//     timestamp: string; // ISO string
+//     sessionId: string;
+//     ip?: string;
+//     country?: string;
+//     city?: string;
+//     region?: string;
+//     latitude?: number;
+//     longitude?: number;
+//     sdkVersion: string;
+//     appVersion: string;
+//     userAgent: string;
+//     url: string;
+//     stackTrace?: string;
+// }
+
 export interface LogEvent {
-    id: string;
-    deviceId?: string;
+    deviceId: string;
     level: "debug" | "info" | "warn" | "error";
     message: string;
-    timestamp: string; // ISO string
-    sessionId: string;
-    ip?: string;
-    country?: string;
-    city?: string;
-    region?: string;
+
+    timestamp: string;
+
+    browser: string;
+    browserVersion?: string;
+
+    deviceName?: string;
+    os?: string;
+
     latitude?: number;
     longitude?: number;
-    sdkVersion: string;
-    appVersion: string;
-    userAgent: string;
+
     url: string;
-    stackTrace?: string;
 }
